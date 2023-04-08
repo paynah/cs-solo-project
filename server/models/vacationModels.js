d037115249507e5acc7cb25565f608eb3f5edc21
Nancy Yu <nancyyu09@gmail.com>
2023-04-07 20:31:40 -0700

    removed db info from vacationModels, pulling from env file instead

--- server/models/vacationModels.js
+++ server/models/vacationModels.js
@@ -1,6 +1,7 @@
 const { Pool } = require('pg');
+require('dotenv').config();
 
-const PG_URI = 'postgres://fiuyetgw:vs7o4rrUi6BsPiTz9H3tF_8TsRIGBkzl@baasu.db.elephantsql.com/fiuyetgw';
+const PG_URI = process.env.PG_URI;
 
 // create a new pool here using the connection string above
 const pool = new Pool({
